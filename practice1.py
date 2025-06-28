df4d26e9496e7241513e4bc19f8c74d3b9b599fa
Andy Gu <andygu.7685@gmail.com>
2025-06-27 18:34:18 -0700

    add median fn

--- practice1.py
+++ practice1.py
@@ -1,1 +1,12 @@
+import math
 print("Hello World")
+def median(input):
+  isOdd = len(input) % 2
+  half = len(input) // 2
+  if isOdd:
+    return input[half]
+  else:
+    return (input[half] + input[half - 1]) / 2
+
+my_list = [0, 1, 2, 6, 4, 9]
+print(median(my_list))
